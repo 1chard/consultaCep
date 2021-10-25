7056744bcda58d6db593eb1340d27e610400dc94
1chard <1chard@github.com>
2021-10-25 16:27:49 -0300

    erro de cpf arrumado

--- cep.js
+++ cep.js
@@ -10,10 +10,10 @@ document.getElementById('cep').addEventListener('change', async(e) => {
     if(/^[0-9]{5}-?[0-9]{3}$/.test(e.target.value)){
         const infoCep = await pesquisarCep(e.target.value);
 
-        document.getElementById('endereco').value = infoCep.logradouro
-        document.getElementById('bairro').value = infoCep.bairro
-        document.getElementById('cidade').value = infoCep.localidade
-        document.getElementById('estado').value = infoCep.uf
+        document.getElementById('endereco').value = infoCep.logradouro ?? ''
+        document.getElementById('bairro').value = infoCep.bairro ?? ''
+        document.getElementById('cidade').value = infoCep.localidade ?? ''
+        document.getElementById('estado').value = infoCep.uf ?? ''
     }
     else
         alert('erro')
